27cbb0d9195951ea487c51bb008341ee24f4f6a6
Dmitry Shavlukevich <dmitry.sh@caspel.by>
2024-08-13 21:04:34 +0300

    add konva

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainPage from "../pages/MainPage/MainPage";
+import styles from "./App.module.scss"
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
